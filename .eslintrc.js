a01f5a8d34d3dc1ef84ead178fcbfd861553f522
Sierk Beij <sierk@riseup.net>
2018-12-11 22:56:26 -0800

    Customized eslint / prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,31 @@
+const restrictedGlobals = require('confusing-browser-globals')
+
 module.exports = {
-  extends: 'react-app',
+  root: true,
+  parser: 'typescript-eslint-parser',
+  extends: ['airbnb'],
+  plugins: ['import', 'jsx-a11y', 'react', 'typescript'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    // The ESLint browser environment defines all browser globals as valid,
+    // even though most people don't know some of them exist (e.g. `name` or `status`).
+    // This is dangerous as it hides accidentally undefined variables.
+    // We blacklist the globals that we deem potentially confusing.
+    // To use them, explicitly reference them, e.g. `window.name` or `window.status`.
+    'no-restricted-globals': ['error'].concat(restrictedGlobals),
+    semi: ['error', 'never'],
+  },
 }
